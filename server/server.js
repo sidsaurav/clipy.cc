2c1c11e429e4f18b9ea2876231951142e26207b8
Siddharth Saurav <sid.saurav11@gmail.com>
2023-06-06 15:03:22 +0530

    update .env location

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ import express from 'express'
 import dotenv from 'dotenv'
 import connectDB from './config/db.js'
 
-dotenv.config()
+dotenv.config({ path: '../config.env' })
 const app = express()
 
 connectDB()
